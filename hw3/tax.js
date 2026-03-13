a96c4c3e4f7979f1a69c3e77f34a9f8c47345d85
Aupain <rasitvichforprogram@gmail.com>
2026-03-13 11:08:44 +0700

    calculate tax

---
+++ hw3/tax.js
@@ -0,0 +1,48 @@
+let count = 1;
+
+function add() {
+  if (count == 1) {
+    document.getElementById("income2").style.display = "block";
+    count++;
+  } else if (count == 2) {
+    document.getElementById("income3").style.display = "block";
+    count++;
+  }
+}
+
+function remove() {
+  if (count == 3) {
+    document.getElementById("income3").style.display = "none";
+    count--;
+  } else if (count == 2) {
+    document.getElementById("income2").style.display = "none";
+    count--;
+  }
+}
+
+function calculate() {
+  let income1 = Number(document.getElementById("income").value) || 0;
+  let income2 = Number(document.getElementById("income2").value) || 0;
+  let income3 = Number(document.getElementById("income3").value) || 0;
+
+  let total = income1 + income2 + income3;
+
+  document.getElementById("total-income").value = total;
+
+  let ratio = 0;
+
+  if (total <= 150000) ratio = 0;
+  else if (total <= 300000) ratio = 5;
+  else if (total <= 500000) ratio = 10;
+  else if (total <= 750000) ratio = 15;
+  else if (total <= 1000000) ratio = 20;
+  else if (total <= 2000000) ratio = 25;
+  else if (total <= 5000000) ratio = 30;
+  else ratio = 35;
+
+  document.getElementById("tax-ratio").value = ratio;
+
+  let tax = (total * ratio) / 100;
+
+  document.getElementById("tax").value = tax;
+}
