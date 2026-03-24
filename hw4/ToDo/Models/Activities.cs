92fce86501b7087099bd3de55c51a9e52bf30db7
Aupain <rasitvichforprogram@gmail.com>
2026-03-24 10:31:00 +0700

    hw4

---
+++ hw4/ToDo/Models/Activities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDo.Models
+{
+    public partial class Activities
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? UserId { get; set; }
+        public DateTime? When { get; set; }
+    }
+}
---
+++ hw4/ToDo/Models/ToDoDbContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ToDo.Models
+{
+    public partial class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext()
+        {
+        }
+
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Activities> Activities { get; set; } = null!;
+        public virtual DbSet<Users> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=hw4;password=hw4;database=hw4db", Microsoft.EntityFrameworkCore.ServerVersion.Parse("12.2.2-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_general_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Activities>(entity =>
+            {
+                entity.ToTable("activities");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.UserId)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("userId");
+
+                entity.Property(e => e.When)
+                    .HasColumnType("datetime")
+                    .HasColumnName("when");
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(100)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Username)
+                    .HasMaxLength(100)
+                    .HasColumnName("username");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
