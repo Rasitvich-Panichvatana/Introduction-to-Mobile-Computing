170625585d2dbe2f8a92fe01ebc1516b02b444a4
Aupain <rasitvichforprogram@gmail.com>
2026-03-28 16:29:36 +0700

    style

--- hw5/hw5/src/components/SelectActionCard.jsx
+++ hw5/hw5/src/components/SelectActionCard.jsx
@@ -31,7 +31,7 @@ function SelectActionCard() {
         width: '100%',
         display: 'grid',
         gridTemplateColumns: 'repeat(auto-fill, minmax(min(200px, 100%), 1fr))',
-        gap: 2,
+        gap: 3,
       }}
     >
       {cards.map((card, index) => (
