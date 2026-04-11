7d8098f7070c7ac91bdc14e18f4857879caea175
Aupain <rasitvichforprogram@gmail.com>
2026-04-11 12:18:07 +0700

    activityApi

---
+++ ToDo-mobile/ToDoMobile/lib/api/activityApi.ts
@@ -0,0 +1,42 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const BASE_URL = "http://localhost:5555";
+
+// Get token
+const getToken = async () => {
+  return await AsyncStorage.getItem("token");
+};
+
+// Common headers
+const getHeaders = async () => {
+  const token = await getToken();
+
+  return {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+};
+
+// Handle response
+const handleResponse = async (res: Response) => {
+  if (res.status === 204) return [];
+
+  if (!res.ok) {
+    const text = await res.text();
+    throw new Error(text || "Request failed");
+  }
+
+  return res.json();
+};
+
+// GET all Activities
+export const getActivities = async () => {
+  const headers = await getHeaders();
+
+  const res = await fetch(BASE_URL, {
+    method: "GET",
+    headers,
+  });
+
+  return handleResponse(res);
+};
